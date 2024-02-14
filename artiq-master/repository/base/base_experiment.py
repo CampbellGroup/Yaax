e889499a5129623be50060e85e197e55563ef3be
Hfarhat <145071110+HmFarhat@users.noreply.github.com>
2024-02-14 14:07:41 -0800

    Update base_experiment.py

--- artiq-master/repository/base/base_experiment.py
+++ artiq-master/repository/base/base_experiment.py
@@ -9,33 +9,6 @@ class YaaxExperiment(Experiment, YaaxStart):
         super().build()
         self._build_components()
         self.build_exp()
-
-    """
-    #This class starts all of the "science" components
-    def _build_components(self):
-        self.setattr_device("core")
-        
-        #Place urukuls here 
-        self.setattr_device("urukul0_ch0")  #369nmdp
-        self.setattr_device("urukul0_ch1")  #411nmdp
-        self.setattr_device("urukul1_ch0")  #935nmdp
-        self.setattr_device("urukul1_ch1")  #?
-        
-        #Place ttls here 
-        self.setattr_device("ttl0")  #PMT TTL
-        
-    @kernel
-    def _initialize(self):
-        self.core.reset() 
-        self.urukul0_ch0.cpld.init()
-        self.urukul0_ch0.init()
-        self.urukul1_ch0.cpld.init()
-        self.urukul1_ch0.init()
-
-        # Set TTLS
-        self.ttl0.input()
-        #print("devices are on")
-    """
     
      #This is class will get over written by the experiment
     def build_exp(self):
